296afac3b7b027c13b33a6d12ade3577ffbcc3ef
kolin52 <138013058+kolin52@users.noreply.github.com>
2023-07-13 14:49:58 +0300

    Update l4-10.py

--- .github/classroom/homework/lecture4/l4-10.py
+++ .github/classroom/homework/lecture4/l4-10.py
@@ -2,17 +2,14 @@
 #години. Забележка: За първите две години, една кучешка година е равна на 10.5 човешки.
 #След това всяка следваща кучешка година се равнява на 4-ри човешки години.
 
-# formula for dog age - dog_age = a / 10.5 + b / 4
 
-# Human life 
-
-hum_age = float(input('Insert years: '))
-if hum_age <= 21:
-    dog_age = float(hum_age/10.5)
+age = int(input('Insert years: '))
+if age <= 2:
+    dog_age = int(age * 10.5)
     print('Dog is ', dog_age, 'years old')
     
 else:
-    dog_age = float(hum_age - 21)/4 + 2
-    print('Dog is ', dog_age, 'years old')
+    dog_age = int(age - 2) * 4 + 21
+    print('Dog age ', dog_age)
 
     
